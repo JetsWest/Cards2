226c26fff00c15d2c925cee7149aefe0f4d141b9
JetsWest <31777630+JetsWest@users.noreply.github.com>
2018-03-14 12:12:11 -0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -5,130 +5,52 @@
  */
 package Game;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import model.Deck;
-
+import org.boof.Output;
+import org.boof.ListItemInput;
 /**
  *
  * @author atkins4440j
  */
 public class Game {
     private Deck deck;
+    private static ArrayList<Player> players;
     private Scanner sc = new Scanner(System.in);
-    private Player you;
-    private Player dealer;
-    private boolean youDone;
-    private boolean dealerDone;
+    
     public Game(){
-        this.youDone = false;
-        this.dealerDone = false;
         this.deck = new Deck();
         this.deck.shuffle();
-        this.you = new Player();
-        this.you.clearHand();
-        this.dealer = new Player();
-        this.dealer.clearHand();
+        this.players = new ArrayList<>();
     }
     public Deck getDeck(){
         return this.deck;
     }
     public void run(){
-        System.out.println("1) Play");
-        System.out.println("2) Quit");
-        System.out.println("3) Check stats");
-        int choice = sc.nextInt();
-        switch(choice){
-            case 1:
-                play();
-            break;
-            case 2:
-                System.exit(0);
-            break;
-            case 3:
-                System.out.println("Wins: " + this.you.numWins());
-                System.out.println("Losses: " + this.you.numLosses());
-                run();
-            break;
-            default:
-                System.out.println("no thanks");
-                run();
-            break;
-        }
-    }
-    public void play(){
-        you.takeCard(deck.drawCard());
-        you.takeCard(deck.drawCard());
-        dealer.takeCard(deck.drawCard());
-        dealer.takeCard(deck.drawCard());
-        System.out.println("here is your deck and its value");
-        you.printHand();
-        System.out.println(you.getHandValue());
-        System.out.println("Here is the dealer's first card");
-        dealer.peek();
-       while(!this.youDone || !this.dealerDone){
-            if (!this.youDone){
-                this.playerTurn();
+        boolean playing = true;
+        System.out.println("Welcome to the 21 game!");
+        ListItemInput wantToPlay = new ListItemInput();
+        wantToPlay.add("y", "Yes");
+        wantToPlay.add("n", "No");
+        while(playing){
+            System.out.println("Want to play?");
+            wantToPlay.run();
+            if (wantToPlay.getKey().equalsIgnoreCase("y")){
+                for (Player player : players){
+                    player.play(this.deck);
+                }   
+            }else{
+                System.out.println("Thanks for playing!");
+                playing = false;
             }
-            else if (!this.dealerDone){
-                this.dealerTurn();
-            }
-           System.out.println();
-        }
-        if (!blackjack()){
-            getWinner();
-        }
-        System.out.println("Play again? 1) Yes 2) No");
-        int answer = sc.nextInt();
-        if (answer == 1){
-            run();
-        }else{
-            System.exit(0);
         }
     }
-    public void getWinner() {
-        if (youDone && dealerDone) {
-            if (you.getHandValue() > dealer.getHandValue() && you.getHandValue() <= 21 || dealer.busted()) {
-                System.out.println("You have won!");
-                you.win();
-                dealer.lose();
-            } else if (this.dealer.getHandValue() > 21 && this.you.getHandValue() > 21) {
-                System.out.println("No one wins the game!");
-            }
-        } else if (dealer.getHandValue() > you.getHandValue() && dealer.getHandValue() <= 21 || you.busted()) {
-            System.out.println("Dealer won!");
-            dealer.win();
-            you.lose();
-        }
-    }
-    public void playerTurn(){
-        System.out.println("Hit or stay? 1) Hit 2) Stay");
-        int hitstay = sc.nextInt();
-        if (hitstay == 1) {
-            you.takeCard(deck.drawCard());
-            System.out.println("Here is your deck and your score");
-            you.printHand();
-            System.out.println(you.getHandValue());
-        }
-        if (you.getHandValue() > 21){
-            System.out.println("Busted!");;
-            this.youDone = true;
-        }
-        if (hitstay == 2){
-            this.youDone = true;
-            System.out.println("You chose to stay, or are forced to");
-        }
-    }
-    public void dealerTurn(){
-        System.out.println("Dealers turn");
-        if (dealer.getHandValue() < 17){
-            dealer.takeCard(deck.drawCard());
-            System.out.println("The dealer has chosen to hit");
-        } else{
-            this.dealerDone = true;
-            System.out.println("The dealer has chosen to stay, with his final value being " + this.dealer.getHandValue());
-        }
-    }
-    public boolean blackjack(){
-        return this.you.getHandValue() == 21 || this.dealer.getHandValue() == 21;
+
+    public static void main(String[] args){
+        Game game = new Game();
+        Player user = new Player();
+        players.add(user);
+        game.run();
     }
 }
--- Player.java
+++ Player.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import model.*;
-
+import org.boof.ListItemInput;
+import org.boof.UI;
+import org.boof.Output;
 public class Player {
+
     private int wins;
     private int losses;
     private int points;
@@ -18,10 +21,8 @@ public class Player {
     public static final int POINTS_PER_GAME = 20;
     private Scanner sc = new Scanner(System.in);
     private List<Card> hand;
-   // private Game game;
 
-    public Player(){
-        //this.game = game;
+    public Player() {
         this.score = 0;
         this.points = 0;
         this.hand = new ArrayList<>();
@@ -39,12 +40,12 @@ public class Player {
         this.score -= POINTS_PER_GAME;
     }
 
-    public void takeCard(Card card){
+    public void takeCard(Card card) {
         this.hand.add(card);
     }
 
-    public boolean busted(){
-        if (getHandValue() > 21){
+    public boolean busted() {
+        if (getHandValue() > 21) {
             return true;
         }
         return false;
@@ -53,44 +54,66 @@ public class Player {
     public int getHandValue() {
         boolean ace = false;
         for (int i = 0; i < this.hand.size(); i++) {
-            if (this.hand.get(i).getRank().value >= 10) {
+            if (this.hand.get(i).getRank().value > 10) {
                 points += 10;
-            }
-            else if (this.hand.get(i).getRank().value == 1) {
+            } else if (this.hand.get(i).getRank().value == 1) {
                 ace = true;
-                if (ace == true && points + 11 <= 21) {
-                    points += 11;
-            }
             } else {
                 points += this.hand.get(i).getRank().value;
             }
+            if (ace == true && points + 11 <= 21) {
+                points += 11;
+            }
+
         }
         return points;
     }
 
-    public void clearHand(){
+    public void clearHand() {
         this.hand.clear();
     }
-    public void peek(){
-        System.out.println(this.hand.get(0));
-    }
-    public void printHand(){
-        for (int i = 0; i < this.hand.size(); i++){
-            System.out.println((i+1) + (":")+this.hand.get(i));
+
+
+    public void play(Deck deck) {
+        boolean isDone = false;
+        if (this.getHandValue() > 21){
+            System.out.println("You have busted!");
+            isDone = true;
+            this.lose();
+        }
+        takeCard(deck.drawCard());
+        takeCard(deck.drawCard());
+        System.out.println("Here are your cards and your score:");
+        System.out.println(this.hand.toString());
+        System.out.println("Score: " + getHandValue());
+        ListItemInput hitOrPass = new ListItemInput();
+        hitOrPass.add("h", "hit");
+        hitOrPass.add("p", "pass");
+        while (!isDone){
+            System.out.println("Hit or pass?");
+            hitOrPass.run();
+            if (hitOrPass.getKey().equalsIgnoreCase("h")) {
+                String result = "";
+                this.takeCard(deck.drawCard());
+                result += "You hand is now " + this.hand.toString() + "\n";
+                result += "Your score is now " + this.getHandValue();
+                System.out.println(result);
+            } else {
+                System.out.println("You have chosen to pass.");
+                isDone = true;
+            }
         }
     }
-    public void play(){
-
-    }
-    public int numWins(){
+ 
+    public int numWins() {
         return this.wins;
     }
 
-    public int numLosses(){
+    public int numLosses() {
         return this.losses;
     }
 
-    public int numScore(){
+    public int numScore() {
         return this.score;
     }
 }
